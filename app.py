43205eed03242736b13410c17422e707990ff548
Musfiq Shohan <musfuq14shohan@gmail.comm>
2020-05-31 22:39:43 +0600

    chnagezssdf

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import credentials, requests
 from flask import Flask, request
 import gunicorn
 app = Flask(__name__)
-
+# Callback URL= "https://mind-flare-chatbot.herokuapp.com/webhook"   for deploying from heroku
 WEBHOOK_VERIFY_TOKEN="Mind-Flare-507163"
 PAGE_TOKEN= "EAAINYZCdZAxdwBAG25N4YfcNSgTb7MvZCiQwB3rkxOQu5AjnBHe3tRUDIs7sN3ZAoXL0ibagz7vSU0ZCawm1UWfJClDJOgNcjzWgIaYPTZAal2UR9RysL5HaReYtPrdT8ZAK0wbMbXPgcG8UFOHxURN4ZACsZBp8AYdsJHhZB9QYL29gZDZD"
 # Adds support for GET requests to our webhook
@@ -14,6 +14,7 @@ def webhook():
     verify_token = request.args.get("hub.verify_token")
     # Check if sent token is correct
     # if verify_token == credentials.WEBHOOK_VERIFY_TOKEN:
+    print("verify token",verify_token)
     if verify_token == WEBHOOK_VERIFY_TOKEN:
         # Responds with the challenge token from the request
         return request.args.get("hub.challenge")
@@ -26,25 +27,39 @@ def webhook():
 def webhook_handle():
     data = request.get_json()
     print("Printing",data)
-    message = data['entry'][0]['messaging'][0]['message']
-    sender_id = data['entry'][0]['messaging'][0]['sender']['id']
+    if 'message' in data['entry'][0]['messaging'][0]:
+        message = data['entry'][0]['messaging'][0]['message']
+        sender_id = data['entry'][0]['messaging'][0]['sender']['id']
 
-    if message['text']:
-        request_body = {
-            'recipient': {
-                'id': sender_id
-            },
-            'message': {"text": "hello, world123456 I am fixed!"}
-        }
-        response = requests.post('https://graph.facebook.com/v5.0/me/messages?access_token=' + PAGE_TOKEN,
-                                 json=request_body).json()
-        return response
+        if message['text']:
+            request_body = {
+                'recipient': {
+                    'id': sender_id
+                },
+                'message': {"text": "hello, shohan I am fixed!"}
+                # 'message': {
+                #     "type":"text",
+                #     "payload":{
+                #         "url":"https://www.facebook.com/musfiq.shohan.1",
+                #         "is_reusable":True
+                #     }
+                # }
+
+            }
+            response = requests.post('https://graph.facebook.com/v5.0/me/messages?access_token=' + PAGE_TOKEN,
+                                     json=request_body).json()
+            return response
+        return 'ok'
+
     return 'ok'
+
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World123!'
+    return 'Hello, World123456!'
 
 if __name__ == "__main__":
+
     app.run(threaded=True, port=5000)
+
